e77a6a0a02ee8cf1119f5caf9e1222610e64329f
njchristian <njchristian@neo.tamu.edu>
2014-02-04 09:37:46 -0600

    Adding Header Files to Master

---
+++ DatabaseObjectTestMain.cpp
@@ -0,0 +1,31 @@
+#include "Database.h"
+
+
+int main(){
+
+	Database d;
+
+	/*
+	Test functionality for 
+	Create table
+	Update
+	Insert
+	Delete
+	Show
+	Exit
+
+	AND
+
+	Selection
+	Projection
+	Rename
+	Set Union
+	Set Difference
+	Cross Product
+	Join
+
+	*/
+
+
+	return 1;
+}
