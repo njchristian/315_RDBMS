7839c760b8372f8e3b954483b14680ad9dc0342a
njchristian <njchristian@neo.tamu.edu>
2014-02-06 16:27:51 -0600

    Added Conditions and conditionlists

--- ConditionList.h
+++ ConditionList.h
@@ -1,4 +1,5 @@
 #include "Condition.h"
+#include "Connector.h"
 #include "Relation.h"
 #include <linkedlist>
 
@@ -52,11 +53,53 @@ public:
 
 	bool evalOnTuple(int tupleIndex){
 	
-		for(Condition::Iterator i = conditions.begin(); i != conditions.end(); i++){
+		LinkedList<Condition> localConditions = conditions;
+	
+		for(Condition::Iterator i = localConditions.begin(); i != localConditions.end(); i++){
 		
-			if(
+			if(i->firstIsVar(){
+				i->setOperand1Entry(r.getEntry(tupleIndex, i->getVar1Index));
+			}
+			
+			if(i->secondIsVar(){
+				i->setOperand2Entry(r.getEntry(tupleIndex, i->getVar2Index));
+			}
+			
+			i->eval();
 			
 		}
+	
+		while(highestPriority > 0){
+		
+			for(Condition::Iterator i = localConditions.begin(); i != localConditions.end(); i++){
+				
+				if(i->getPriority == highestPriority){
+					Condition::Iterator next = i.next();
+					
+					
+					//update current value
+					switch (i->getConnector()){
+					case (AND):
+						i->setLiteral( i->getLiteral() && next->getLiteral());
+						i->setConnector(next->getConnector());
+						conditions.remove(next);
+						break;
+					case (OR):
+						i->setLiteral( i->getLiteral() || next->getLiteral());
+						i->setConnector(next->getConnector());
+						conditions.remove(next);
+						break;
+					case (NONE):
+						break;
+					}
+					
+				}
+				
+			}
+		
+		}
+		
+		return conditions.front().getLiteral();
 	
 	}
 
