87258ca6e13047717df576d4c75d6a377bf263a2
njchristian <njchristian@neo.tamu.edu>
2014-02-09 13:10:04 -0600

    Integrate Changes

--- Database.h
+++ Database.h
@@ -37,14 +37,14 @@ public:
 	void addTupleToRelation( vector<Entry> tuple, string relationName );
 
 	Relation crossProduct( Relation* targetRelationA, Relation* targetRelationB );
-	Relation crossProduct( Relation* targetRelationA, string relationB ){ crossProduct(targetRelationA, findRelation(relationB)); }
-	Relation crossProduct( string relationA, Relation* targetRelationB ){ crossProduct(findRelation(relationA), targetRelationB); }
-	Relation crossProduct( string relationA, string relationB ){ crossProduct(findRelation(relationA) , findRelation(relationB)); }
+	Relation crossProduct( Relation* targetRelationA, string relationB ){ return crossProduct(targetRelationA, findRelation(relationB)); }
+	Relation crossProduct( string relationA, Relation* targetRelationB ){ return crossProduct(findRelation(relationA), targetRelationB); }
+	Relation crossProduct( string relationA, string relationB ){ return crossProduct(findRelation(relationA) , findRelation(relationB)); }
 
 	Relation differenceTwoRelation( Relation* targetRelationA, Relation* targetRelationB );
-	Relation differenceTwoRelation( Relation* targetRelationA, string relationB ){ differenceTwoRelation(targetRelationA, findRelation(relationB)); }
-	Relation differenceTwoRelation( string relationA, Relation* targetRelationB ){ differenceTwoRelation(findRelation(relationA), targetRelationB); }
-	Relation differenceTwoRelation( string relationA, string relationB ){ differenceTwoRelation(findRelation(relationA) , findRelation(relationB)); }
+	Relation differenceTwoRelation( Relation* targetRelationA, string relationB ){ return differenceTwoRelation(targetRelationA, findRelation(relationB)); }
+	Relation differenceTwoRelation( string relationA, Relation* targetRelationB ){ return differenceTwoRelation(findRelation(relationA), targetRelationB); }
+	Relation differenceTwoRelation( string relationA, string relationB ){ return differenceTwoRelation(findRelation(relationA) , findRelation(relationB)); }
 
 	int findAttribute( );
 
@@ -53,25 +53,25 @@ public:
 	int findTuple( );
 
 	Relation naturalJoin( Relation* targetRelationA, Relation* targetRelationB);
-	Relation naturalJoin( Relation* targetRelationA, string relationB ){ naturalJoin(targetRelationA, findRelation(relationB)); }
-	Relation naturalJoin( string relationA, Relation* targetRelationB ){ naturalJoin(findRelation(relationA), targetRelationB); }
-	Relation naturalJoin( string relationA, string relationB ){ naturalJoin(findRelation(relationA) , findRelation(relationB)); }
+	Relation naturalJoin( Relation* targetRelationA, string relationB ){ return naturalJoin(targetRelationA, findRelation(relationB)); }
+	Relation naturalJoin( string relationA, Relation* targetRelationB ){ return naturalJoin(findRelation(relationA), targetRelationB); }
+	Relation naturalJoin( string relationA, string relationB ){ return naturalJoin(findRelation(relationA) , findRelation(relationB)); }
 
 	Relation projection( vector<string> attributeNames, Relation* targetRelation );
-	Relation projection( vector<string> attributeNames, string relationName ){ projection(attributeNames, findRelation(relationName)); }
+	Relation projection( vector<string> attributeNames, string relationName ){ return projection(attributeNames, findRelation(relationName)); }
 
 	void removeTupleFromRelation( );
 
 	Relation renameAttributes( vector<string> newNames, Relation* targetRelation );
-	Relation renameAttributes( vector<string> newNames, string relationName ){ renameAttributes( newNames, findRelation(relationName)); }
+	Relation renameAttributes( vector<string> newNames, string relationName ){ return renameAttributes( newNames, findRelation(relationName)); }
 	
 	Relation selection( vector<Condition>, Relation* targetRelation);
-	Relation selection( vector<Condition> conditions, string targetRelationName ){ selection( conditions, findRelation( targetRelationName)); }
+	Relation selection( vector<Condition> conditions, string targetRelationName ){ return selection( conditions, findRelation( targetRelationName)); }
 
 	Relation unionTwoRelations( Relation* targetRelationA, Relation* targetRelationB );
-	Relation unionTwoRelations( Relation* targetRelationA, string relationB ){ unionTwoRelations(targetRelationA, findRelation(relationB)); }
-	Relation unionTwoRelations( string relationA, Relation* targetRelationB ){ unionTwoRelations(findRelation(relationA), targetRelationB); }
-	Relation unionTwoRelations( string relationA, string relationB ){ unionTwoRelations(findRelation(relationA) , findRelation(relationB)); }
+	Relation unionTwoRelations( Relation* targetRelationA, string relationB ){ return unionTwoRelations(targetRelationA, findRelation(relationB)); }
+	Relation unionTwoRelations( string relationA, Relation* targetRelationB ){ return unionTwoRelations(findRelation(relationA), targetRelationB); }
+	Relation unionTwoRelations( string relationA, string relationB ){ return unionTwoRelations(findRelation(relationA) , findRelation(relationB)); }
 
 };
 
