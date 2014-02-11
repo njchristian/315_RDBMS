a406db56407acc67c3213d73318dbc7c99a62355
njchristian <njchristian@neo.tamu.edu>
2014-02-10 22:18:05 -0600

    Update fixed

--- Database.h
+++ Database.h
@@ -77,7 +77,7 @@ public:
 	Relation unionTwoRelations( string relationA, Relation* targetRelationB ){ return unionTwoRelations(findRelation(relationA), targetRelationB); }
 	Relation unionTwoRelations( string relationA, string relationB ){ return unionTwoRelations(findRelation(relationA) , findRelation(relationB)); }
 
-	void update( string relationName, string attributeName, string testCondition, Operation op, Entry newValue );
+	Relation* update(string relationName, vector<string> attributeName, vector<Entry> newVal, vector<Condition> conditions);
 
 };
 
