1a93ec17adf704cd1d371a50eda58e484960711b
chrisdlangham <chris.langham32@gmail.com>
2014-02-17 19:24:53 -0600

    made Parser::getRelation public to make test easier

--- Parser.h
+++ Parser.h
@@ -24,9 +24,6 @@ class Parser{
 		views.push_back( r );
 	}
 
-	//Get a relation from views or database
-	Relation getRelation( string r );
-
 	//Is c alpha numeric or '_'?
 	bool isAlphaNum( int c );
 
@@ -174,6 +171,8 @@ public:
 
 	int parse( string command );
 
+	//Get a relation from views or database
+	Relation getRelation( string r );
 
 };
 
